cdf5d42e97cf23c5cdf9028215c5be6232ffcae9
YomnaTarek <yomnaragab13@gmail.com>
2021-09-29 15:28:35 +0200

    Added Numbered Pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Pagination from './components/Pagination';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <button type="button" className="btn btn-danger">
+            <Link className="nav-link h3 text-light " to="/pagination">Numbered Pagination</Link>
+          </button>
+          <button type="button" className="btn btn-primary">
+            <Link className="nav-link h3 text-light" to="/infinite">Infinite Scrolling</Link>
+          </button>
+          <h3 className="p-3"> Click on one of the buttons to view its functionality!</h3>
+          <Switch>
+            <Route path="/pagination">
+              <Pagination />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
